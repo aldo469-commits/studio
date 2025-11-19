89ad5a48368fc6bb0cbf6deb22450eb7f9f016fa
Alejandro Doncel Padilla <aldo469@vidalibarraquer.net>
2025-11-19 19:27:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3198175275-336a6",
+  "appId": "1:208523796808:web:ec638d983fe3f47fde537d",
+  "apiKey": "AIzaSyAsnO27xMX8iZVlNV5rMrM9nFpDotbRMoU",
+  "authDomain": "studio-3198175275-336a6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "208523796808"
+};

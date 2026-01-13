80af3a9673e34c8a68baacec9561fdafa4f7828f
Alejandro Doncel Padilla <aldo469@vidalibarraquer.net>
2026-01-13 14:27:25 +0000

    EN LA CARPETA PUBLIC HAY UNA IMAGEN LLAMADA LOGO.PNG, PONLA COMO IMAGEN

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,10 +5,11 @@ export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center gap-2", className)}>
         <Image 
-            src="/LOGO" 
+            src="/LOGO.png" 
             alt="EJA GlobalTrans Logo" 
             width={40} 
             height={40} 
+            className="object-contain"
         />
         <span className="font-headline font-bold text-xl text-foreground">
             EJA GlobalTrans
@@ -21,13 +22,13 @@ export function FooterLogo({ className }: { className?: string }) {
     return (
         <div className={cn("flex items-center gap-2", className)}>
              <Image 
-                src="/LOGO" 
+                src="/LOGO.png" 
                 alt="EJA GlobalTrans Logo" 
                 width={40} 
                 height={40}
-                className="brightness-0 invert" 
+                className="brightness-0 invert object-contain" 
             />
-            <span className="font-headline font-bold text-xl text-card">
+            <span className="font-headline font-bold text-xl text-primary-foreground">
                 EJA GlobalTrans
             </span>
         </div>
